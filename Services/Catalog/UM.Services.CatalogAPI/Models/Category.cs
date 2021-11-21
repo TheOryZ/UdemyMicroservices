40ad51c895967ca73c2eb423ca54a1ce06b96435
TheOryZ <kadirrturan@gmail.com>
2021-11-21 19:18:20 +0300

    Added notes
    Added notes

--- Services/Catalog/UM.Services.CatalogAPI/Models/Category.cs
+++ Services/Catalog/UM.Services.CatalogAPI/Models/Category.cs
@@ -5,8 +5,8 @@ namespace UM.Services.CatalogAPI.Models
 {
     public class Category
     {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonId]    //: this property is Identity and will be produced automatically
+        [BsonRepresentation(BsonType.ObjectId)]     //: mongoDb type
         public string Id { get; set; }
         public string Name { get; set; }
     }
--- Services/Catalog/UM.Services.CatalogAPI/Models/Course.cs
+++ Services/Catalog/UM.Services.CatalogAPI/Models/Course.cs
@@ -6,21 +6,21 @@ namespace UM.Services.CatalogAPI.Models
 {
     public class Course
     {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonId]    //: this property is Identity and will be produced automatically
+        [BsonRepresentation(BsonType.ObjectId)]     //: mongoDb type
         public string Id { get; set; }
         public string Name { get; set; }
-        [BsonRepresentation(BsonType.Decimal128)]
+        [BsonRepresentation(BsonType.Decimal128)]   //: mongoDb type
         public decimal Price { get; set; }
         public string Description { get; set; }
         public string UserId { get; set; }
         public string Picture { get; set; }
-        [BsonRepresentation(BsonType.DateTime)]
+        [BsonRepresentation(BsonType.DateTime)]     //: mongoDb type
         public DateTime CreatedTime { get; set; }
         public Feature Feature { get; set; }
         [BsonRepresentation(BsonType.ObjectId)]
         public string CategoryId { get; set; }
-        [BsonIgnore]
+        [BsonIgnore]    //: that it does not create on the database side
         public Category Category { get; set; }
     }
 }
