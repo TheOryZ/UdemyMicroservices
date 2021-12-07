6a24421e2f4964d4c9bcc8903ef269ae9195ad5d
TheOryZ <kadirrturan@gmail.com>
2021-12-08 00:01:25 +0300

    --comm
    --comm

---
+++ IdentityServer/UM.IdentityServer/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UM.IdentityServer.Dtos;
+using UM.IdentityServer.Models;
+using UM.Shared.SharedLib.Dtos;
+
+namespace UM.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpDto signUpDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signUpDto.UserName,
+                Email = signUpDto.Email,
+                City = signUpDto.City
+            };
+            var result = await _userManager.CreateAsync(user, signUpDto.Password);
+            if(!result.Succeeded)
+            {
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 400));
+            }
+            return NoContent();
+        }
+    }
+}
---
+++ IdentityServer/UM.IdentityServer/Dtos/SignUpDto.cs
@@ -0,0 +1,10 @@
+﻿namespace UM.IdentityServer.Dtos
+{
+    public class SignUpDto
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string City { get; set; }
+    }
+}
